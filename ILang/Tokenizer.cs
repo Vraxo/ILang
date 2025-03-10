6f188468af16a55f65c55201115913bfe15263e5
Parsa <Parsa@DESKTOP-32S8QLS>
2025-03-10 22:44:59 +0330

    Failed

--- ILang/Tokenizer.cs
+++ ILang/Tokenizer.cs
@@ -32,7 +32,7 @@ public class Tokenizer
                 {
                     tokens.Add(token); // Add as boolean literal
                 }
-                else if (token is "if" or "else" or "fun" or "let" or "while")
+                else if (token is "if" or "else" or "fun" or "let" or "while" or "num_to_string")
                 {
                     tokens.Add(token); // Add as keyword
                 }
@@ -82,6 +82,12 @@ public class Tokenizer
             {
                 tokens.Add("!=");
                 currentIndex += 2;
+            }
+            // Handle single-character comparison operators (e.g., '<', '>')
+            else if (currentChar == '<' || currentChar == '>')
+            {
+                tokens.Add(currentChar.ToString());
+                currentIndex++;
             }
             // Handle single-character symbols
             else
